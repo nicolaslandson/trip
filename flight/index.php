7d39603b31c62eb84fcb00d933a5aab01523f569
nmlchkv <48715986+nmlchkv@users.noreply.github.com>
2025-06-10 10:46:56 +0300

    Create index.php

---
+++ flight/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url=https://aviasales.tp.st/s2IQNhZd" />
+    <script>
+      window.location.href = "https://aviasales.tp.st/s2IQNhZd";
+    </script>
+  </head>
+  <body>
+    Redirecting...
+  </body>
+</html>
